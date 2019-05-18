e65b97fc0ea42c9e24d03cb362fa5fc7a70383c9
motylla <magda.waliszewska@gmail.com>
2019-05-18 10:25:38 +0200

    first commit

---
+++ src/main/java/pl/sda/servlets/RawServlet.java
@@ -0,0 +1,16 @@
+package pl.sda.servlets;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class RawServlet implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+}
