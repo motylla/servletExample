56d6d46c4506060de314b331d4e2e1bc24a1455a
motylla <magda.waliszewska@gmail.com>
2019-05-18 12:48:16 +0200

    cookie servlet example added

--- src/main/java/pl/sda/servlets/CookieServlet.java
+++ src/main/java/pl/sda/servlets/CookieServlet.java
@@ -1,4 +1,37 @@
 package pl.sda.servlets;
 
-public class CookieServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name="CookieServlet", urlPatterns = "/cookie")
+public class CookieServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        PrintWriter out = resp.getWriter();
+        out.println("<form action = '/cookie' method = 'post' >");
+        out.println("<select name = 'color'> ");
+        out.println("<option>Blue</option>");
+        out.println("<option>Pink</option>");
+        out.println("<option>Green</option>");
+        out.println("<option>Black</option>");
+        out.println("</select>");
+        out.println("<input type='submit' value='zapisz'>");
+        out.println("</form>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String color = req.getParameter("color");
+        resp.addCookie(new Cookie("color", color));
+        resp.getWriter().println("Ciasteczko zapisane");
+    }
 }
